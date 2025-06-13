50501d97eb6b5f4f4d15f219eca5755000dca2fe
singhvi28 <akkisinghvi28@gmail.com>
2025-06-14 00:36:47 +0530

    authentication tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,41 @@ const axios = require("axios");
 const BACKEND_URL = "http://localhost:3000"
 const WS_URL = "ws://localhost:3001"
 
+const axios = {
+    post: async (...args) => {
+        try {
+            const res = await axios2.post(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+    get: async (...args) => {
+        try {
+            const res = await axios2.get(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+    put: async (...args) => {
+        try {
+            const res = await axios2.put(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+    delete: async (...args) => {
+        try {
+            const res = await axios2.delete(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+}
+
 describe("Authentication", () => {
     test('User is able to sign up only once', async () => {
         const username = "kirat" + Math.random(); // kirat0.12331313
