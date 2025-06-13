0e634bbc769de38f08549ba75a37f1eb2256b4f2
singhvi28 <akkisinghvi28@gmail.com>
2025-06-14 00:38:09 +0530

    User metadata endpoint tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -107,3 +107,75 @@ describe("Authentication", () => {
         expect(response.status).toBe(403)
     })
 })
+
+describe("User metadata endpoint", () => {
+    let token = "";
+    let avatarId = ""
+
+    beforeAll(async () => {
+       const username = `kirat-${Math.random()}`
+       const password = "123456"
+
+       await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        username,
+        password,
+        type: "admin"
+       });
+
+       const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+        username,
+        password
+       })
+
+       token = response.data.token
+
+       const avatarResponse = await axios.post(`${BACKEND_URL}/api/v1/admin/avatar`, {
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+            "name": "Timmy"
+        }, {
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        })
+        console.log("avatarresponse is " + avatarResponse.data.avatarId)
+
+        avatarId = avatarResponse.data.avatarId;
+
+    })
+
+    test("User cant update their metadata with a wrong avatar id", async () => {
+        const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+            avatarId: "123123123"
+        }, {
+            headers: {
+                "authorization": `Bearer ${token}`
+            }
+        })
+
+        expect(response.status).toBe(400)
+    })
+
+    test("User can update their metadata with the right avatar id", async () => {
+        const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+            avatarId
+        }, {
+            headers: {
+                "authorization": `Bearer ${token}`
+            }
+        })
+
+        expect(response.status).toBe(200)
+    })
+
+    test("User is not able to update their metadata if the auth header is not present", async () => {
+        const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+            avatarId
+        })
+
+        expect(response.status).toBe(403)
+    })
+
+    test("test 3", () => {
+        
+    })
+});
